653338776911cd1a787a022ab6660948983348a0
Noctyris <noctyris@proton.me>
2025-08-31 22:04:23 +0200

    Upgrade mouse detection

--- actions.c
+++ actions.c
@@ -8,16 +8,25 @@ int is_mouse_between(pos_t a, pos_t b, pos_t mouse) {
   pos_t max = a.x < b.x ? b : a;
 
   for (int x = 1; x < dx; x++) {
-    for (int i = -2; i < 3; i++) {
-      int pX = min.x + x, pY = min.y + D * x + i;
-      if (mouse.x == pX && mouse.y == pY)
-        return 1;
-    }
-    for (int i = -2; i < 3; i++) {
-      int pX = min.x + x + i, pY = min.y + D * x;
-      if (mouse.x == pX && mouse.y == pY)
-        return 1;
-    }
+    int pX = min.x + x, pY = min.y + D * x;
+    if (pX == mouse.x && pY == mouse.y)
+      return 1;
+    if (pX == mouse.x + 1 && pY == mouse.y + 1)
+      return 1;
+    if (pX == mouse.x && pY == mouse.y + 1)
+      return 1;
+    if (pX == mouse.x - 1 && pY == mouse.y + 1)
+      return 1;
+    if (pX == mouse.x - 1 && pY == mouse.y)
+      return 1;
+    if (pX == mouse.x - 1 && pY == mouse.y - 1)
+      return 1;
+    if (pX == mouse.x && pY == mouse.y - 1)
+      return 1;
+    if (pX == mouse.x + 1 && pY == mouse.y - 1)
+      return 1;
+    if (pX == mouse.x + 1 && pY == mouse.y)
+      return 1;
   }
 
   return 0;
